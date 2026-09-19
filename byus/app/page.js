dee906404c131a84967141cde0401b22abceeb11
evanryon-jpg <evanryon@yahoo.com>
2026-09-19 16:02:46 -0400

    Show the right walkthrough action for each visitor
    * Make walkthrough CTA account-aware
    
    * Pass homepage session to creator walkthrough
    
    * Point fans to clearly labeled example creator pages
    
    * Add anchor for example creator pages

--- byus/app/page.js
+++ byus/app/page.js
@@ -43,7 +43,7 @@ export default async function HomePage() {
       <FeedbackWidget />
       <LiveActivityTicker />
       <Hero user={session} />
-      <CreatorWalkthrough />
+      <CreatorWalkthrough user={session} />
       <CreatorShowcase />
       <EarningsCalculator />
       <Features />
